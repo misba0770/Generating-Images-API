43298f9d6a093a971184e5ed9320b5b005fc5a8e
misba0770 <80534061+misba0770@users.noreply.github.com>
2022-02-22 18:37:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const input= document.getElementById('input');
+const grid= document.getElementsByClassName('grid')[0];
+input.addEventListener('keydown',function(event){
+if(event.key ==='Enter')
+loadImg();
+})
+
+function loadImg(){
+  removeImages();
+
+  const url='https://api.unsplash.com/search/photos/?query='+input.value+'&per_page=9&client_id=tIE1QfRzTRLOa8X--qxcDGsYfT8klYIz0l9AxpXc_Tw';
+
+
+  fetch(url)
+  .then(response =>{
+      if(response.ok)return response.json();
+      else alert(response.status);
+  })
+  .then(data =>{
+      const imageNodes=[];
+      
+      for(let i =0; i<data.results.length ;i++){
+          imageNodes[i]=document.createElement('div');
+          imageNodes[i].className='img';
+          imageNodes[i].style.backgroundImage='url('+data.results[i].urls.raw+')';
+          imageNodes[i].addEventListener('dblclick',function(){
+          window.open(data.results[i].links.download,'_blank');
+          })
+          grid.appendChild(imageNodes[i]);
+      
+    }
+  })
+}
+
+
+function removeImages(){
+    grid.innerHTML=' ';
+
+}
